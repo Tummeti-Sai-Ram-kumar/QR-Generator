57d64418b93e81b6d6b3fa7af7098bb1e838e5f1
Tummeti-Sai-Ram-kumar <56332777+Tummeti-Sai-Ram-kumar@users.noreply.github.com>
2022-05-17 13:42:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var qrimg = document.getElementById("qr-img");
+function AddQR(){
+    var text = document.getElementById("input-field").value;
+    
+    if(text==''){
+        window.alert("Enter text or url");
+    }
+    else{
+        document.getElementById("gen-btn").innerText = "Making QR Code ....";
+        document.getElementById("QR-Box").classList.add("expand");
+        
+        qrimg.style.opacity = "1";
+        qrimg.setAttribute("src",`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`);
+        qrimg.addEventListener('load',() => {
+            document.getElementById("gen-btn").innerText = "Generated QR Code ";
+        })
+    }
+    
+}
+
+
+
+function Shrink(){
+    var input = document.getElementById("input-field").value;
+    document.getElementById("gen-btn").innerText = "Generate QR Code";
+    if(input=='')
+    qrimg.style.opacity = "0";
+}
